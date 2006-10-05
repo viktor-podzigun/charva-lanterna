5dabbf736cc2d9ecbaeffd57d8bd49955db2e5ba
robpitman <robpitman@4f353515-93dd-dd11-abb4-001558a53407>
2006-10-05 17:56:03 +0000

    Add FocusAdapter class
    git-svn-id: svn://svn.code.sf.net/p/charva/code/charva/trunk@41 4f353515-93dd-dd11-abb4-001558a53407

---
+++ java/src/charva/awt/event/FocusAdapter.java
@@ -0,0 +1,33 @@
+package charva.awt.event;
+
+/**
+ * An abstract adapter class for receiving keyboard focus events.
+ * The methods in this class are empty. This class exists as convenience for creating listener objects.<p/>
+ *
+ * Extend this class to create a FocusEvent listener and override the methods for the events of interest.
+ * (If you implement the FocusListener interface, you have to define all of the methods in it.
+ * This abstract class defines null methods for them all, so you can only have to define methods for
+ * events you care about.)<p/>
+ *
+ * Create a listener object using the extended class and then register it with a component using the
+ * component's addFocusListener method. When the component gains or loses the keyboard focus, the relevant
+ * method in the listener object is invoked, and the FocusEvent is passed to it.
+ */
+public class FocusAdapter implements FocusListener {
+
+    /**
+     * Invoked when a component gains the keyboard focus.
+     * @param fe_
+     */
+    public void focusGained(FocusEvent fe_) {
+
+    }
+
+    /**
+     * Invoked when a component loses the keyboard focus.
+     * @param fe_
+     */
+    public void focusLost(FocusEvent fe_) {
+
+    }
+}
