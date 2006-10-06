99a136832aabaef8fb28c335c4af8603627ab7b3
robpitman <robpitman@4f353515-93dd-dd11-abb4-001558a53407>
2006-10-06 19:54:59 +0000

    When a window is opened, it should post a WINDOW_OPENED event.
    git-svn-id: svn://svn.code.sf.net/p/charva/code/charva/trunk@46 4f353515-93dd-dd11-abb4-001558a53407

--- java/src/charva/awt/Window.java
+++ java/src/charva/awt/Window.java
@@ -145,6 +145,9 @@ public class Window
 
         this.draw();
 
+        WindowEvent we = new WindowEvent(this, AWTEvent.WINDOW_OPENED);
+        _term.getSystemEventQueue().postEvent(we);
+
         /* Rather than call Toolkit.sync() directly here, we put a SyncEvent
          * onto the SyncQueue. The SyncThread will read it off the SyncQueue
          * and then sleep for 50msec before putting the SyncEvent onto
