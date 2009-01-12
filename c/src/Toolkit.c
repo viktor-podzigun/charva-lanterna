0e03f204bf09eb3dbb6c6b4d0bdf7d037befbdf4
lapo <lapo@4f353515-93dd-dd11-abb4-001558a53407>
2009-01-12 14:05:42 +0000

    Added likely forgotten attron/attroff lines in drawBoxNative.
    git-svn-id: svn://svn.code.sf.net/p/charva/code/charva/branches/andxor@112 4f353515-93dd-dd11-abb4-001558a53407

--- c/src/Toolkit.c
+++ c/src/Toolkit.c
@@ -372,6 +372,8 @@ JNIEXPORT void JNICALL Java_charva_awt_Toolkit_drawBoxNative
     if (colors_started)
 	    attr = COLOR_PAIR(colorpair_);
 
+    attron(attr);
+
     // If the top of the box is outside the clipping rectangle, don't bother
     // to draw the top.
     if (top_ >= top && top_ <= bottom) {
@@ -414,6 +416,8 @@ JNIEXPORT void JNICALL Java_charva_awt_Toolkit_drawBoxNative
 	        my_addch_with_clip(ACS_VLINE);   // right vertical line
 	    }
     }
+
+    attroff(attr);
 }
 
 JNIEXPORT void JNICALL Java_charva_awt_Toolkit_blankBoxNative
