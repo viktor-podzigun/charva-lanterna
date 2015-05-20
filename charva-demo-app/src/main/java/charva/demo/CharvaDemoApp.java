4da9523440d18658dba64749ec43489dc0f478ba
Viktor Podzigun <viktor.podzigun@ecabo.de>
2015-05-20 18:09:27 +0200

    Added external run for charva-demo-app

--- charva-demo-app/src/main/java/charva/demo/CharvaDemoApp.java
+++ charva-demo-app/src/main/java/charva/demo/CharvaDemoApp.java
@@ -17,29 +17,46 @@
 
 package charva.demo;
 
+import java.awt.GraphicsEnvironment;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import charva.awt.event.ActionEvent;
 import charva.awt.event.ActionListener;
 import charva.showcase.Tutorial;
 import charva.toolkit.lanterna.LanternaToolkit;
 import charvax.swing.SwingUtilities;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.ansi.CygwinTerminal;
+import com.googlecode.lanterna.terminal.ansi.UnixTerminal;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalColorConfiguration;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalDeviceConfiguration;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
 
 /**
  * Defines entry point for the charva-lanterna demo application.
  */
-public class CharvaDemoApp {
+public final class CharvaDemoApp {
+
+    private CharvaDemoApp() {
+    }
 
     public static void main(String[] args) throws IOException {
-        final DefaultTerminalFactory factory = new DefaultTerminalFactory();
+        new CharvaDemoApp().run();
+    }
+
+    private void run() throws IOException {
+        //final DefaultTerminalFactory factory = new DefaultTerminalFactory();
         //factory.setSuppressSwingTerminalFrame(true);
 
-        final LanternaToolkit toolkit = new LanternaToolkit(
-                factory.createTerminal());
-
+        final LanternaToolkit toolkit = new LanternaToolkit(createTerminal());
         toolkit.startEventThread();
 
         SwingUtilities.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 final Tutorial tutorial = new Tutorial();
                 tutorial.setExitActionListener(new ActionListener() {
@@ -50,5 +67,71 @@ public class CharvaDemoApp {
                 tutorial.show();
             }
         });
+    }
+
+    private Terminal createTerminal() throws IOException {
+        final OutputStream outputStream = System.out;
+        final InputStream inputStream = System.in;
+        final Charset charset = Charset.forName(
+                System.getProperty("file.encoding"));
+
+        if (GraphicsEnvironment.isHeadless()) {
+            if (isOperatingSystemWindows()) {
+                return new WinTerminal(inputStream, outputStream, charset);
+            }
+
+            return new UnixTerminal(inputStream, outputStream, charset);
+        }
+
+        SwingTerminalFrame swingTerminalFrame = new SwingTerminalFrame(
+                "charva-lanterna Demo",
+                SwingTerminalDeviceConfiguration.DEFAULT,
+                SwingTerminalFontConfiguration.DEFAULT,
+                SwingTerminalColorConfiguration.DEFAULT,
+                SwingTerminalFrame.AutoCloseTrigger.CloseOnExitPrivateMode);
+
+        swingTerminalFrame.setVisible(true);
+        return swingTerminalFrame;
+    }
+
+    /**
+     * Detects whether the running platform is Windows* by looking at the
+     * operating system name system property
+     */
+    private static boolean isOperatingSystemWindows() {
+        return System.getProperty("os.name", "").toLowerCase()
+                .startsWith("windows");
+    }
+
+    private static class WinTerminal extends CygwinTerminal {
+
+        public WinTerminal(InputStream terminalInput,
+                OutputStream terminalOutput,
+                Charset terminalCharset) throws IOException {
+
+            super(terminalInput, terminalOutput, terminalCharset);
+        }
+
+        @Override
+        protected String getSTTYCommand() {
+            return findProgram("stty.exe");
+        }
+
+        @Override
+        protected String getShellCommand() {
+            return findProgram("sh.exe");
+        }
+
+        private static String findProgram(String programName) {
+            String[] paths = System.getProperty("java.library.path").split(";");
+            for (String path : paths) {
+                final File shBin = new File(path, programName);
+                if (shBin.exists()) {
+                    return shBin.getAbsolutePath();
+                }
+            }
+
+            return programName;
+        }
     }
 }
